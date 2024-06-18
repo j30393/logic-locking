d09d886c60b6374b72fe9e80287e46bde6b96088
j30393 <petersu@gapp.nthu.edu.tw>
2024-06-18 22:01:56 +0800

    fixed: let the code runnable at the server

--- baseline/src/main.cpp
+++ baseline/src/main.cpp
@@ -1,16 +1,16 @@
 #include "./inc/node.h"
 #include "./inc/encryption.h"
 int main(int argc, char* argv[]){
-	std::srand((unsigned)9487);
+	std::srand((unsigned) time(NULL));
 	encryption* enc = new encryption();
 	if(argc == 3)
 		enc->setOutputname(argv[2]);
 	enc->readfile(argv[1]);
 	enc->setDebugMode(false);
-	enc->set_test_num(5);
+	enc->set_test_num(100); // number of testcase for fault impact 
 	enc->topological_sort();
 	enc->fault_impact_cal();
-	enc->key_ratio = 0.2;
+	enc->key_ratio = 1; // ratio of number of key bits
 	enc->xor_encryption();
 	enc->outputfile();
 	return 0;
