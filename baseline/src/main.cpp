87ca5bf00566b0a0f3831f226acc116b26295645
erichwu11 <erichwu11@gmail.com>
2024-06-24 23:25:22 +0800

    fix: fix errors form easy check

--- baseline/src/main.cpp
+++ baseline/src/main.cpp
@@ -13,7 +13,8 @@ int main(int argc, char* argv[]){
 	enc->key_ratio = 1; // ratio of number of key bits
 	//enc->xor_encryption();
 	enc->setDebugMode(1);
-	enc->sl_one_encryption();
+	//enc->sl_one_encryption();
+	enc->sl_compare_encryption();
 	enc->outputfile();
 	return 0;
 }
